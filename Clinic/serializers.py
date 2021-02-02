1c1756e9e53bbd2355e6c84991aef06ae6e7ba27
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-02 17:25:00 +0530

    Add New Clinic Serializer

---
+++ Clinic/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import *
+
+class ClinicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clinic
+        fields= "__all__"
