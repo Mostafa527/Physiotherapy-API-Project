ef516f1915b5163bc11fca9857a37cc42c52b707
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-02 18:40:00 +0530

    Add New Clinic Model & Update Clinic Views Class

--- Clinic/models.py
+++ Clinic/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class Clinic(models.Model):
+    Name=models.CharField(max_length=200,unique=True)
+    Address=models.CharField(max_length=300)
+    Contact=models.CharField(max_length=100)
+    Email=models.CharField(max_length=150,unique=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.Name
--- Clinic/urls.py
+++ Clinic/urls.py
@@ -1,7 +1,6 @@
 
-from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path
+from .views import *
 urlpatterns = [
     path('Clinic', ClinicList.as_view(), name='clinic_list')
 ]
