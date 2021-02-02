5612d50f31cbe7f25fac213d5e450ac2f5073f2c
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-02 19:10:00 +0530

    Create new PostgreSQL Database & Install Rest_framework

--- Physiotherapy/settings.py
+++ Physiotherapy/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Physiotherapy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MyPhysioDBProj',
+        'USER': 'postgres',
+        'PASSWORD': '42105518',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
