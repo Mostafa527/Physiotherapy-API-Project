4f999534a31e99d6aba9d6d4e25a48c1e67ea6ca
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-05 13:30:00 +0530

    Create new Class APIVIEW for Clinic GetByID Request

--- Clinic/views.py
+++ Clinic/views.py
@@ -2,6 +2,20 @@ from rest_framework import status
 from rest_framework.response import Response
 from .serializers import *
 from rest_framework.views import APIView
+from django.http import Http404
+
+class clinic_detail(APIView):
+    def get_object(self, pk):
+        try:
+            return Clinic.objects.get(pk=pk)
+        except Clinic.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        clinic = self.get_object(pk)
+        serializer = ClinicSerializer(clinic)
+        return Response(serializer.data)
+
 class ClinicList(APIView):
     def get(self,request):
         clinics = Clinic.objects.all()
