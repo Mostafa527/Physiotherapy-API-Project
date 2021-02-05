fa4d586e5a19677dd3fc0ece2aa44ccb4ea62332
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-05 13:22:00 +0530

    Create New Clinic URL for PUT Request

--- Clinic/urls.py
+++ Clinic/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('Clinic', ClinicList.as_view(), name='clinic_list')
+    path('Clinic', ClinicList.as_view(), name='clinic_list'),
+    path('Clinic/<int:pk>',clinic_detail.as_view(),name='clinic_detail')
 ]
--- Clinic/views.py
+++ Clinic/views.py
@@ -1,4 +1,4 @@
-
+from rest_framework import status
 from rest_framework.response import Response
 from .serializers import *
 from rest_framework.views import APIView
@@ -7,6 +7,13 @@ class ClinicList(APIView):
         clinics = Clinic.objects.all()
         data=ClinicSerializer(clinics,many=True).data
         return Response(data)
+
+    def post(self, request):
+        serializer = ClinicSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
