3e28c8f810a80676c12117534fe16014ae896eff
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-07 15:18:00 +0530

    Adding New Model for Accounts with attributes

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,22 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class NewUser(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True,null=True)
+    last_name = models.CharField(max_length=150, blank=True,null=True)
+    password2 = models.CharField(max_length=120)
+    Address = models.CharField(max_length=300)
+    Contact = models.CharField(max_length=25)
+    BirthDate =models.DateField(auto_now=False, blank=True, null=True)
+    start_date = models.DateTimeField(default=timezone.now)
+    about = models.TextField(_(
+        'about'), max_length=500, blank=True)
+
+    def __str__(self):
+        return self.username
+
