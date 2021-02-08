54771fd127d3431a7d3632cdb1f92815247646c8
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-08 21:20:00 +0530

    Update Settings add New App for Accounts

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,7 +1,8 @@
 
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-
+from django.utils.translation import gettext_lazy as _
 USER_TYPE_CHOICES = (
     ('admin', 'admin'),
     ('doctor', 'doctor'),
