1c3c8721fe2bcd1038ece5bb72ec37c6d50eee21
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-08 23:22:00 +0530

    Account Views & URLS

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
 
-# Create your views here.
+from .models import *
+from .serializers import *
+from rest_framework.views import APIView
+from django.http import Http404
+from .serializers import UserSerializer
+
+from rest_framework.response import Response
+
+
+class RegisterView(APIView):
+    def get(self, request):
+        admins = NewUser.objects.all()
+        data = UserSerializer(admins, many=True).data
+        return Response(data)
+
+
+
+class user_detail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return NewUser.objects.get(pk=pk)
+        except NewUser.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = UserSerializer(snippet)
+        print(snippet.pk)
+        return Response(serializer.data)
+
+
