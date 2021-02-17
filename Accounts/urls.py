474fcde20d387a240bb51e627d47e8c52e94399c
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-17 19:20:00 +0530

    Create new Login URL

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
+  path('login/', LoginView.as_view(), name='login'),
   path('user_detail/<int:pk>', user_detail.as_view(),name='user_detail'),
   path('user_list/', RegisterView.as_view(), name="register"),
 
