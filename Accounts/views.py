15c8d0ce5cdc34859cc717d65eb7b34880d4657d
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-17 12:20:00 +0530

    Update Accounts_Views add New Delete Request

--- Accounts/views.py
+++ Accounts/views.py
@@ -38,7 +38,7 @@ class user_detail(APIView):
         serializer = UserSerializer(snippet)
         print(snippet.pk)
         return Response(serializer.data)
-    
+
     def put(self, request, pk, format=None):
         snippet = self.get_object(pk)
         serializer = UserSerializer(snippet, data=request.data)
@@ -47,4 +47,9 @@ class user_detail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
