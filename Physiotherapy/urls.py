5df769b8a0dfcafeb3c353617dd349d1d3785164
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-17 17:10:00 +0530

    Modify Serializer

--- Physiotherapy/urls.py
+++ Physiotherapy/urls.py
@@ -4,5 +4,6 @@ from django.urls import path,include
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-    path('', include('Clinic.urls'))
+    path('', include('Clinic.urls')),
+    path('', include('Accounts.urls'))
 ]
