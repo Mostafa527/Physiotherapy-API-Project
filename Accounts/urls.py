9cf3f4719f7d5da3c7b18b56bf8ba6cf9da1beb8
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-17 21:12:00 +0530

    Create URL for Second Logout View

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
   path('user_detail/<int:pk>', user_detail.as_view(),name='user_detail'),
   path('user_list/', RegisterView.as_view(), name="register"),
   path('Logout/', Logout.as_view(), name='logout'),
-  
+  path('user_logout/', LogoutView.as_view(), name='logout_view')
+
 
 ]
 
