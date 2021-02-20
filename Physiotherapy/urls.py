2c6c73913679bff4d3b52f1e6943062d4dc5adab
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-20 18:18:00 +0530

    Update Settings & Add Accounts URL

--- Physiotherapy/urls.py
+++ Physiotherapy/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,include
 urlpatterns = [
    path('admin/', admin.site.urls),
     path('', include('Clinic.urls')),
-    path('', include('Accounts.urls'))
+    path('', include('Accounts.urls')),
+    path('', include('MyAdmin.urls'))
 ]
