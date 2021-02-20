c25b4685a53abe0027a75aafdda6db13e0bc0f5d
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-20 20:18:00 +0530

    Migrate MyAdmin

--- MyAdmin/urls.py
+++ MyAdmin/urls.py
@@ -2,9 +2,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-path('MyAdmin/<int:pk>',admin_detail.as_view(),name='admin_detail'),
-path('MyAdmin',adminList.as_view(),name='admin_list'),
+#path('MyAdmin/<int:pk>',admin_detail.as_view(),name='admin_detail'),
+#path('MyAdmin',adminList.as_view(),name='admin_list'),
 ]
-urlpatterns += [
-    path('api-auth/', include('rest_framework.urls'))
-]
+
+
