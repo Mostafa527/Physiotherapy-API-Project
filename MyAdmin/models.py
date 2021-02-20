f37289f8fee2e8400c03c388e6056747a72e82fa
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-20 16:20:00 +0530

    Create New Admin Model

--- MyAdmin/models.py
+++ MyAdmin/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from Clinic.models import Clinic
+from Accounts.models import NewUser
+class MyAdmin(models.Model):
+    user = models.OneToOneField(NewUser, on_delete=models.CASCADE, primary_key=True, related_name='admin_user')
 
-# Create your models here.
+    Admin_Clinic =models.ForeignKey(Clinic,related_name='admins',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.username
