2f4ffb66034b434745e84fa81f0fb8a2a071e6ce
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-23 14:12:00 +0530

    Update Class AdminProfileSerializer & Add New Method create

--- MyAdmin/serializers.py
+++ MyAdmin/serializers.py
@@ -16,3 +16,14 @@ class AdminProfileSerializer(serializers.ModelSerializer):
         model = MyAdmin
         fields = ('user', 'Admin_Clinic',)
         read_only_fields = ('is_admin','id',)
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_data.is_admin = True
+        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
+        user.is_admin = True
+        user.user_type = 'admin'
+        user.save()
+        myadmin, created = MyAdmin.objects.update_or_create(user=user,
+                                                            Admin_Clinic=validated_data.pop('Admin_Clinic'))
+        return myadmin
