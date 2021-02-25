6b4fd6b31d92ed627bcbf6abcbe0d23f7232435b
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-25 20:20:00 +0530

    Testing AdminDetail URL Routing

--- MyAdmin/urls.py
+++ MyAdmin/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-#path('MyAdmin/<int:pk>',admin_detail.as_view(),name='admin_detail'),
+path('MyAdmin/<int:pk>',admin_detail.as_view(),name='admin_detail'),
 path('MyAdmin',adminList.as_view(),name='admin_list'),
 ]
 
