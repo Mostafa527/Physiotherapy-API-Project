10676ef361b4522cbaeca57912b6dadb16175962
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-25 14:20:00 +0530

    Testing adminList URL

--- MyAdmin/urls.py
+++ MyAdmin/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
 #path('MyAdmin/<int:pk>',admin_detail.as_view(),name='admin_detail'),
-#path('MyAdmin',adminList.as_view(),name='admin_list'),
+path('MyAdmin',adminList.as_view(),name='admin_list'),
 ]
 
 
