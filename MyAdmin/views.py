11b7ed5863670bbe81b7cc70c0241dd1fa855a00
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-25 14:10:00 +0530

    Add Post Request for Accounts_Views

--- MyAdmin/views.py
+++ MyAdmin/views.py
@@ -36,3 +36,10 @@ class adminList(APIView):
         admins = MyAdmin.objects.all()
         data=AdminProfileSerializer(admins,many=True).data
         return Response(data)
+
+    def post(self, request):
+        serializer = AdminProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
