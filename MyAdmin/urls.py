5b1bb1065eb7f38b0487de217a451d39748eabbb
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-02-26 20:20:00 +0530

    Update Accounts URL

--- MyAdmin/urls.py
+++ MyAdmin/urls.py
@@ -6,4 +6,6 @@ path('MyAdmin/<int:pk>',admin_detail.as_view(),name='admin_detail'),
 path('MyAdmin',adminList.as_view(),name='admin_list'),
 ]
 
-
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls'))
+]
