4d4ac631412145b22b1854c0247570e0e20401d5
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-01 13:22:00 +0530

    Add Physiotherapist Model

--- Physiotherapist/models.py
+++ Physiotherapist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from Clinic.models import Clinic
+from Accounts.models import NewUser
+class Physiotherapist(models.Model):
+    user = models.OneToOneField(NewUser, on_delete=models.CASCADE, primary_key=True, related_name='physio_user')
 
-# Create your models here.
+    Clinic_Physio=models.ForeignKey(Clinic,related_name='pysio_therpists',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
