9cd4b8f5f7f08c5cf1a62d287a069b7e6088c341
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-02 15:15:00 +0530

    Update serializers & Add create method

--- Physiotherapist/serializers.py
+++ Physiotherapist/serializers.py
@@ -17,3 +17,12 @@ class PhysioProfileSerializer(serializers.ModelSerializer):
         fields = ('user', 'Clinic_Physio',)
         read_only_fields = ('id','is_doctor',)
 
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
+        user.is_doctor = True
+        user.user_type = 'doctor'
+        user.save()
+        physio, created = Physiotherapist.objects.update_or_create(user=user,
+                                                                   Clinic_Physio=validated_data.pop('Clinic_Physio'))
+        return physio
