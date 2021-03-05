8e760cf323e88a8a2fdc61822a9d9cedd19cb2a9
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-05 13:12:00 +0530

    Add Physio Views

--- Physiotherapist/views.py
+++ Physiotherapist/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from .serializers import *
+from rest_framework.views import APIView
+from django.http import Http404
+
+class physio_detail(APIView):
+    def get_object(self, pk):
+        try:
+            return Physiotherapist.objects.get(pk=pk)
+        except Physiotherapist.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        physio = self.get_object(pk)
+        serializer = PhysioSerializer(physio)
+        return Response(serializer.data)
