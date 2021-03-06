0cb7de84a073b64ad0881acbb5ec9a0bb4d6be44
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-06 15:30:00 +0530

    Solving error & update PUT Request

--- Physiotherapist/views.py
+++ Physiotherapist/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .serializers import *
 from rest_framework.views import APIView
 from django.http import Http404
+from rest_framework import status
 
 class physio_detail(APIView):
     def get_object(self, pk):
@@ -21,6 +22,6 @@ class physio_detail(APIView):
 
         serializer = PhysioProfileSerializer(physio, data=request.data)
         if serializer.is_valid():
-            
+            serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
