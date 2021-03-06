40671b27f16abc0e1740b29c0095e6c1a015dc68
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-06 17:30:00 +0530

    Define DELETE Request for Physiotherapist

--- Physiotherapist/views.py
+++ Physiotherapist/views.py
@@ -25,3 +25,8 @@ class physio_detail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        physio = self.get_object(pk)
+        physio.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
