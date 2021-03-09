b75bee3d4a2dde27a514b9d6795cc9554b7db69f
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-09 15:09:00 +0530

    Adding new url for new request

--- Physiotherapist/urls.py
+++ Physiotherapist/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 path('Physio/<int:pk>',physio_detail.as_view(),name='physio_detail'),
 path('Physio',PhysioList.as_view(),name='physioList'),
+path('Physio/Clinic/<int:clinic_id>',views.therpysDetailsByClinic,name='TherpyDetailsByClinic'),
+
 ]
