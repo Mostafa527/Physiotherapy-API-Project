7e2e2061630fa2cb4d7d9eadd7644c1f5dfffbf4
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-12 22:27:00 +0530

    Add Patient Model & Register App

--- Patient/models.py
+++ Patient/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from Physiotherapist.models import Physiotherapist
+from Accounts.models import NewUser
+class Patient(models.Model):
+    user = models.OneToOneField(NewUser, on_delete=models.CASCADE, primary_key=True, related_name='patient_user')
 
-# Create your models here.
+    Physio_Patient=models.ForeignKey(Physiotherapist,related_name='patients',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
