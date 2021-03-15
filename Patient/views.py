bd984224ec8f9ff69bfd5c213cf3b14ffe244d01
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-15 09:30:00 +0530

    Add Patient Views

--- Patient/views.py
+++ Patient/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from django.http import Http404
+
+class patient_detail(APIView):
+    def get_object(self, pk):
+        try:
+            return Patient.objects.get(pk=pk)
+        except Patient.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        patient = self.get_object(pk)
+        print(patient.pk)
+        serializer = PatientProfileSerializer(patient)
+        return Response(serializer.data)
