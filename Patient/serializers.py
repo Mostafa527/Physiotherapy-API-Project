7be805a0c54d5f454d47772725ccbf9f812edb53
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-17 19:07:00 +0530

    add update serailizer

--- Patient/serializers.py
+++ Patient/serializers.py
@@ -27,5 +27,44 @@ class PatientProfileSerializer(serializers.ModelSerializer):
                                                             Physio_Patient=validated_data.pop('Physio_Patient'))
         return myadmin
 
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user')
+
+
+        myuser = instance.user
+
+        myuser.username = user_data.get(
+            'username',
+            myuser.username
+        )
+
+        myuser.email = user_data.get(
+            'email',
+            myuser.email
+        )
+
+        myuser.first_name = user_data.get(
+            'first_name',
+            myuser.first_name
+        )
+        myuser.setpassword = user_data.get(
+            'password',
+            myuser.password
+        )
+        myuser.is_patient = user_data.get(
+            'is_patient',
+            True
+        )
+        myuser.user_type = user_data.get(
+            'user_type',
+            'patient'
+        )
+
+        myuser.save()
+        instance.user = validated_data.get('user', instance.user)
+        instance.Physio_Patient = validated_data.get('Physio_Patient', instance.Physio_Patient)
+        instance.save()
+        return instance
+
 
 
