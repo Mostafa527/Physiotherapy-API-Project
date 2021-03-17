439c639413f016d69ffd6f89a19c5ea3a88944ad
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-17 15:05:00 +0530

    add create serailizer

--- Patient/serializers.py
+++ Patient/serializers.py
@@ -17,4 +17,15 @@ class PatientProfileSerializer(serializers.ModelSerializer):
         fields = ('user', 'Physio_Patient',)
         read_only_fields = ('id','is_patient',)
 
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
+        user.is_patient = True
+        user.user_type = 'patient'
+        user.save()
+        myadmin, created = Patient.objects.update_or_create(user=user,
+                                                            Physio_Patient=validated_data.pop('Physio_Patient'))
+        return myadmin
+
+
 
