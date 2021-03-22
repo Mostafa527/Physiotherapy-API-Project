b413bb5dbb3faf6f1cd1ef15bb670c79ffa8f110
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-22 13:30:00 +0530

    Add new request in Clinic to get all physiotherapist in clinic

--- Clinic/urls.py
+++ Clinic/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 from .views import *
+from . import views
 urlpatterns = [
+    path('Clinic/Patient/<int:patient_id>',views.ClinicDetailsByPat,name='ClinicDetailsByPatient'),
     path('Clinic', ClinicList.as_view(), name='clinic_list'),
     path('Clinic/<int:pk>',clinic_detail.as_view(),name='clinic_detail')
 ]
