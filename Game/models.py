000af23b5de32b7883fbab518b829ac961b89ddd
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-25 14:06:00 +0530

    Create Game Model

--- Game/models.py
+++ Game/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from Accounts.models import NewUser
+class Game(models.Model):
+    Name=models.CharField(max_length=80,unique=True)
+    Description=models.CharField(max_length=250)
+    owner =models.ForeignKey(NewUser,related_name='Game_Owner',on_delete=models.CASCADE)
 
-# Create your models here.
+    def __str__(self):
+        return self.Name
