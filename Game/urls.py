bad26c80c60daeb60644e7ace8d212bd70945f1a
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-03-27 18:31:00 +0530

    Add New URL for Game

--- Game/urls.py
+++ Game/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
-
+from Game import views
 urlpatterns = [
-
+path('Game',GametList.as_view(),name='game_list'),
+path('Game/<int:p>',game_detail.as_view(),name='game_detail'),
 ]
