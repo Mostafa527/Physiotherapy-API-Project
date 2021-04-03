111194d9a2d1a2e633e900ccab8fe774e4c22e86
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-04-03 16:05:00 +0530

    create new game views

--- Game/views.py
+++ Game/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+
+from .models import *
+from .serializers import *
+from rest_framework.views import APIView
+from rest_framework import status
+from django.http import Http404
+from rest_framework import permissions
+from rest_framework import generics
+from Game.permissions import IsOwnerOrReadOnly
+
+
+
+class game_detail(APIView):
+    def get_object(self, pk):
+        try:
+            return Game.objects.get(pk=pk)
+        except Game.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        game = self.get_object(pk)
+        serializer = GameSerializer(game)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        game = self.get_object(pk)
+
+        serializer = GameSerializer(game, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        game = self.get_object(pk)
+       
+        return Response(status=status.HTTP_204_NO_CONTENT)
