1e496bb09bedb8f0973eee1591310905b4807c74
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-04-04 18:23:00 +0530

    Add new GameList

--- Game/views.py
+++ Game/views.py
@@ -32,3 +32,16 @@ class game_detail(APIView):
         game = self.get_object(pk)
         game.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class GametList(APIView):
+    def get(self,request):
+        games = Game.objects.all()
+        data=GameSerializer(games,many=True).data
+        return Response(data)
+
+    def post(self, request):
+        serializer = GameSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
