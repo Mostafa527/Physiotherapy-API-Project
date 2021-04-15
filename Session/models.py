d364a6e37d7d06115c5fc12a5d5daa5c8b0ba864
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-04-15 17:17:00 +0530

    Create new Model

--- Session/models.py
+++ Session/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from ExercisePlan.models import Exercise_Plan
+class Session(models.Model):
+    DateOfCreation=models.DateTimeField(blank=True,null=True)
+    Session_Plan=models.ForeignKey(Exercise_Plan,related_name='session_plan',on_delete=models.CASCADE)
 
-# Create your models here.
+    def __str__(self):
+        return str(self.id)
