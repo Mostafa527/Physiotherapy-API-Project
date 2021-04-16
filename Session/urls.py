9cc80dcf4f2cecfb67228e48de792601d2f53870
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-04-16 06:15:00 +0530

    Create Session URLS

--- Session/urls.py
+++ Session/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
 
-
+path('Session/ExercisePlan/<int:exerciseplan_id>',views.SessionByExercisePlan,name='SessionByExercisePlan'),
+path('Session/<int:session_id>',views.session_detail,name='session_detail'),
+path('Session',SessionList.as_view(),name='session_list'),
 ]
