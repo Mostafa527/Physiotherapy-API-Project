2364c42e3de61720fc0c1f110c447f7a3f2b27ea
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-04-18 08:09:00 +0530

    Create new Scores Model (Table)

--- Scores/models.py
+++ Scores/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from Session.models import Session
 
-# Create your models here.
+#Scores Table
+class Scores(models.Model):
+    DateOfCreation=models.DateTimeField(blank=True,null=True)
+    ScorePoints=models.PositiveIntegerField(blank=False)
+    PainAngles=models.CharField(max_length=300,blank=False)
+    Session_Status=models.CharField(max_length=50,blank=False)
+    Percentage=models.CharField(max_length=30,blank=False)
+    Session_Score=models.ForeignKey(Session,related_name='session_score',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
