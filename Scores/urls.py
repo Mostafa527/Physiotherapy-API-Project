620b9e0aef49943708b2f5cb89357de0244020bd
Mostafa.Net <mostafakhaled327@yahoo.com>
2021-04-18 14:02:00 +0530

    Add new URLS paths for Scores

--- Scores/urls.py
+++ Scores/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .views import *
 
 urlpatterns = [
-
-
+#Paths
+path('Scores/<int:session_id>',views.ScoresBySessionID,name='ScoresBySessionID'),
+path('Score',ScoreList.as_view(),name='score_list'),
 ]
