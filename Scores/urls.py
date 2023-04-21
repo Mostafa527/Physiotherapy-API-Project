869c8c55f0bb5951b12c6b1e0b2b61c76c86ac62
Mostafa.Net <mostafakhaled327@yahoo.com>
2023-04-21 11:34:00 +0530

    Add Comment

--- Scores/urls.py
+++ Scores/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from .views import *
-
+#URLS Pattern Comment
 urlpatterns = [
 #Paths
 path('Scores/<int:session_id>',views.ScoresBySessionID,name='ScoresBySessionID'),
--- Scores/views.py
+++ Scores/views.py
@@ -11,6 +11,7 @@ from rest_framework.permissions import AllowAny
 #get ScoresUsingSessionID
 @api_view(['GET'])
 def ScoresBySessionID(request,session_id):
+
     try:
         session = Session.objects.get(pk=session_id)
     except Session.DoesNotExist:
@@ -23,3 +24,17 @@ def ScoresBySessionID(request,session_id):
             return Response({'message': 'No Scores'}, status=status.HTTP_404_NOT_FOUND)
         score_serializer =ScoreSerializer(scores,many=True)
         return Response(score_serializer.data)
+
+#Second View
+class ScoreList(APIView):
+    def get(self,request):
+        scores = Scores.objects.all()
+        data=ScoreSerializer(scores,many=True).data
+        return Response(data)
+
+    def post(self,request):
+        serializer = ScoreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
